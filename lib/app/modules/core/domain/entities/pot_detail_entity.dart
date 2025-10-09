72b085fb836d8c4eed0b86aada53fbaaf933216b
andongyeon <a56634368@gmail.com>
2025-10-09 19:33:37 +0900

    feat: pot name

--- lib/app/modules/core/domain/entities/pot_detail_entity.dart
+++ lib/app/modules/core/domain/entities/pot_detail_entity.dart
@@ -2,21 +2,16 @@ import 'package:pot_g/app/modules/chat/domain/enums/pot_status.dart';
 import 'package:pot_g/app/modules/core/domain/entities/pot_summary_entity.dart';
 
 abstract class PotDetailEntity extends PotSummaryEntity {
-  const PotDetailEntity({
+  PotDetailEntity({
     required super.id,
     required super.route,
     required super.startsAt,
     required super.endsAt,
     required super.current,
     required super.total,
-    required this.name,
-    this.departureTime,
-    required this.status,
-    this.accountingRequested = 0,
   });
 
-  final String name;
-  final DateTime? departureTime;
-  final PotStatus status;
-  final int? accountingRequested;
+  DateTime? get departureTime;
+  PotStatus get status;
+  int? get accountingRequested;
 }
