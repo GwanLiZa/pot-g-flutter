7546d9e77091450faadbf40989a2ed37e0213df0
Boseong <lee@paperst.ar>
2025-10-09 12:18:30 +0900

    feat: set departure time (#114)

--- lib/app/modules/chat/data/repositories/websocket_pot_info_repository.dart
+++ lib/app/modules/chat/data/repositories/websocket_pot_info_repository.dart
@@ -1,6 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pot_g/app/modules/chat/data/data_sources/remote/chat_pot_api.dart';
+import 'package:pot_g/app/modules/chat/data/enums/pot_departure_time_result.dart';
+import 'package:pot_g/app/modules/chat/data/models/confirm_departure_time_request_model.dart';
 import 'package:pot_g/app/modules/chat/domain/entities/pot_info_entity.dart';
+import 'package:pot_g/app/modules/chat/domain/exceptions/departure_time_exception.dart';
 import 'package:pot_g/app/modules/chat/domain/repositories/pot_info_repository.dart';
 import 'package:pot_g/app/modules/socket/data/data_sources/websocket.dart';
 import 'package:pot_g/app/modules/socket/data/models/events/pot_event_model.dart';
@@ -40,8 +44,28 @@ class WebsocketPotInfoRepository implements PotInfoRepository {
   }
 
   @override
-  Future<void> setDepartureTime(PotInfoEntity pot, DateTime date) {
-    // TODO: implement setDepartureTime
-    throw UnimplementedError();
+  Future<void> setDepartureTime(PotInfoEntity pot, DateTime date) async {
+    try {
+      final result = await _api.confirmDepartureTime(
+        pot.id,
+        ConfirmDepartureTimeRequestModel(departureTime: date),
+      );
+      switch (result.result) {
+        case PotDepartureTimeResult.ok:
+          return;
+        case PotDepartureTimeResult.notAHost:
+          throw DepartureTimeException.notAHost();
+        case PotDepartureTimeResult.afterDeparture:
+          throw DepartureTimeException.afterDeparture();
+        case PotDepartureTimeResult.beforeNow:
+          throw DepartureTimeException.beforeNow();
+        case PotDepartureTimeResult.potNotExist:
+          throw DepartureTimeException.potNotExist();
+        case PotDepartureTimeResult.potAlreadyClosed:
+          throw DepartureTimeException.potAlreadyClosed();
+      }
+    } on DioException catch (e) {
+      throw DepartureTimeException.networkError(e.error.toString());
+    }
   }
 }
