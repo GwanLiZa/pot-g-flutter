b8369381d9911b3667096d00104992d43f80be9b
andongyeon <a56634368@gmail.com>
2025-10-09 18:52:53 +0900

    feat: add get my pots api

--- lib/app/modules/chat/data/data_sources/remote/chat_pot_api.dart
+++ lib/app/modules/chat/data/data_sources/remote/chat_pot_api.dart
@@ -3,9 +3,11 @@ import 'package:injectable/injectable.dart';
 import 'package:pot_g/app/modules/chat/data/models/confirm_departure_time_request_model.dart';
 import 'package:pot_g/app/modules/chat/data/models/confirm_departure_time_response_model.dart';
 import 'package:pot_g/app/modules/chat/data/models/get_pot_events_query_model.dart';
+import 'package:pot_g/app/modules/chat/data/models/my_pots_model.dart';
 import 'package:pot_g/app/modules/chat/data/models/pot_events_model.dart';
 import 'package:pot_g/app/modules/chat/data/models/pot_info_model.dart';
 import 'package:pot_g/app/modules/core/data/dio/pot_dio.dart';
+import 'package:pot_g/app/modules/core/data/models/pot_detail_model.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'chat_pot_api.g.dart';
@@ -15,6 +17,9 @@ part 'chat_pot_api.g.dart';
 abstract class ChatPotApi {
   @factoryMethod
   factory ChatPotApi(PotDio dio) = _ChatPotApi;
+
+  @GET('me')
+  Future<MyPotsModel> getMyPots();
 
   @GET('{id}/info')
   Future<PotInfoModel> getPotInfo(@Path('id') String id);
