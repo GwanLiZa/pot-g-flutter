3749313870e2eb6ce048b39607943c4430afcf3d
andongyeon <a56634368@gmail.com>
2025-10-11 11:37:02 +0900

    refactor: delete fcm request entity

--- lib/app/modules/core/data/models/fcm_request_model.dart
+++ lib/app/modules/core/data/models/fcm_request_model.dart
@@ -1,13 +1,10 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:pot_g/app/modules/core/domain/entities/fcm_request_entity.dart';
 
 part 'fcm_request_model.freezed.dart';
 part 'fcm_request_model.g.dart';
 
 @freezed
-sealed class FcmRequestModel
-    with _$FcmRequestModel
-    implements FcmRequestEntity {
+sealed class FcmRequestModel with _$FcmRequestModel {
   factory FcmRequestModel({
     required String fcmToken,
     required String os,
